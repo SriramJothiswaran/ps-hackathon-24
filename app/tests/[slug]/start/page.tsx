8159b2a9a7e15ffbc7f03c6bb18027e41b07620b
Sriram Jothiswaran <connect2sriram@hotmail.com>
2024-02-04 07:52:44 +0530

    fixed form submit

--- app/tests/[slug]/start/page.tsx
+++ app/tests/[slug]/start/page.tsx
@@ -63,60 +63,65 @@ const Page = () => {
   ]
   return (
     <div className='bg-gray-100'>
-      <div className='h-screen flex flex-col container p-6 gap-8'>
-        <h3 className='text-xl'>
-          Question {currentQuestion + 1} of {questions.length}
-        </h3>
-        <p className='grow overflow-scroll bg-white p-6 text-lg'>
-          {questions[currentQuestion].question.map((q, i) =>
-            q.element === 'p' ? (
-              <p key={`${currentQuestion}-${i}`} className='pb-4 leading-8'>
-                {q.text}
-              </p>
-            ) : q.element === 'img' ? (
-              <img key={i} src={q.src} alt={q.text} />
-            ) : (
-              ''
-            )
-          )}
-          <p className='text-md mt-6 mb-2'>(Select all applicable answers)</p>
-          <ul className='flex flex-col gap-6 '>
-            {questions[currentQuestion].options.map((o, i) => (
-              <li
-                key={`${i}`}
-                className='flex items-center gap-2 text-lg cursor-pointer'
-              >
-                <input
-                  type='checkbox'
-                  id={`${currentQuestion}-${i}`}
-                  name={`${currentQuestion}-${i}`}
-                />
-                <label
-                  htmlFor={`${currentQuestion}-${i}`}
-                  className='cursor-pointer'
+      <form
+        onSubmit={(e) => {
+          e.preventDefault()
+          e.target.reset()
+          setCurrentQuestion(currentQuestion + 1)
+        }}
+      >
+        <div className='h-screen flex flex-col container p-6 gap-8'>
+          <h3 className='text-xl'>
+            Question {currentQuestion + 1} of {questions.length}
+          </h3>
+          <p className='grow overflow-scroll bg-white p-6 text-lg'>
+            {questions[currentQuestion].question.map((q, i) =>
+              q.element === 'p' ? (
+                <p key={`${currentQuestion}-${i}`} className='pb-4 leading-8'>
+                  {q.text}
+                </p>
+              ) : q.element === 'img' ? (
+                <img key={i} src={q.src} alt={q.text} />
+              ) : (
+                ''
+              )
+            )}
+            <p className='text-md mt-6 mb-2'>(Select all applicable answers)</p>
+            <ul className='flex flex-col gap-6 '>
+              {questions[currentQuestion].options.map((o, i) => (
+                <li
+                  key={`${i}`}
+                  className='flex items-center gap-2 text-lg cursor-pointer'
                 >
-                  {o}
-                </label>
-              </li>
-            ))}
-          </ul>
-        </p>
-        <div className='ml-auto flex gap-4 items-center'>
-          <p className='text-lg text-gray-700'>Remaining Time: 40 min</p>
-          {currentQuestion === questions.length - 1 ? (
-            <Button size='xl' asChild>
-              <Link href='/tests/typescript/result'>Submit</Link>
-            </Button>
-          ) : (
-            <Button
-              size='xl'
-              onClick={() => setCurrentQuestion(currentQuestion + 1)}
-            >
-              Submit & Next
-            </Button>
-          )}
+                  <input
+                    type='checkbox'
+                    id={`${currentQuestion}-${i}`}
+                    name={`${currentQuestion}-${i}`}
+                  />
+                  <label
+                    htmlFor={`${currentQuestion}-${i}`}
+                    className='cursor-pointer'
+                  >
+                    {o}
+                  </label>
+                </li>
+              ))}
+            </ul>
+          </p>
+          <div className='ml-auto flex gap-4 items-center'>
+            <p className='text-lg text-gray-700'>Remaining Time: 40 min</p>
+            {currentQuestion === questions.length - 1 ? (
+              <Button size='xl' asChild>
+                <Link href='/tests/typescript/result'>Submit</Link>
+              </Button>
+            ) : (
+              <Button type='submit' size='xl'>
+                Submit & Next
+              </Button>
+            )}
+          </div>
         </div>
-      </div>
+      </form>
     </div>
   )
 }
