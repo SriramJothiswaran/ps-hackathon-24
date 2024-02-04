d3e15e41dd110929531fef1324373de6f52fc6fa
Sriram Jothiswaran <connect2sriram@hotmail.com>
2024-02-04 13:25:45 +0530

    changed sapient navbar

--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -19,14 +19,14 @@ const Navbar = () => {
   const pathname = usePathname()
 
   return (
-    <Disclosure as='nav' className='bg-primary'>
+    <Disclosure as='nav' className='bg-white border-b'>
       {({ open }) => (
         <>
           <div className='mx-auto max-w-7xl px-2 sm:px-6 lg:px-8'>
-            <div className='relative flex h-16 items-center justify-between'>
+            <div className='relative flex h-20 items-center justify-between'>
               <div className='absolute inset-y-0 left-0 flex items-center sm:hidden'>
                 {/* Mobile menu button*/}
-                <Disclosure.Button className='relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white'>
+                <Disclosure.Button className='relative inline-flex items-center justify-center rounded-md p-2  text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white'>
                   <span className='absolute -inset-0.5' />
                   <span className='sr-only'>Open main menu</span>
                   {open ? (
@@ -39,22 +39,20 @@ const Navbar = () => {
               <div className='flex flex-1 items-center justify-center sm:items-stretch sm:justify-start'>
                 <div className='flex flex-shrink-0 items-center'>
                   <img
-                    className='h-8 w-auto'
-                    src='/MRCL_Logo_public.svg'
+                    className='h-12 w-auto'
+                    src='/ps-logo.svg'
                     alt='Your Company'
                   />
                 </div>
-                <div className='hidden sm:ml-6 sm:block'>
+                <div className='hidden sm:ml-6 sm:block pt-1'>
                   <div className='flex space-x-4'>
                     {navigation.map((item) => (
                       <a
                         key={item.name}
                         href={item.href}
                         className={classNames(
-                          pathname === item.href
-                            ? 'bg-gray-900 text-white'
-                            : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                          'rounded-md px-3 py-2 text-sm font-medium'
+                          pathname === item.href ? '' : 'text-primary',
+                          ' px-3 py-2 text-sm font-medium hover:text-[#fe414d]'
                         )}
                         aria-current={item.current ? 'page' : undefined}
                       >
@@ -67,7 +65,7 @@ const Navbar = () => {
               <div className='absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0'>
                 <button
                   type='button'
-                  className='relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800'
+                  className='relative rounded-full  p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800'
                 >
                   <span className='absolute -inset-1.5' />
                   <span className='sr-only'>View notifications</span>
